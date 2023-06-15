5f83d5dc52229cc088878f371e165d5461afc799
revengermods <136675444+revengermods@users.noreply.github.com>
2023-06-15 09:20:06 +0300

    Add files via upload

--- CraftHelperRodina v1.0.lua
+++ CraftHelperRodina v1.0.lua
@@ -24,7 +24,7 @@ local script_vers_text = "1.00"
 local update_url = "https://raw.githubusercontent.com/revengermods/crafthelper/main/update.ini" -- тут тоже свою ссылку
 local update_path = getWorkingDirectory() .. "/update.ini" -- и тут свою ссылку
 
-local script_url = "" -- тут свою ссылку
+local script_url = "https://github.com/revengermods/crafthelper/raw/main/CraftHelperRodina%20v1.0.lua" -- тут свою ссылку
 local script_path = thisScript().path
 
 function main()
